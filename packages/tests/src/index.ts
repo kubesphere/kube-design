1479ce56fe7d5fa19bfea7cb92028510e973da47
chenz24 <chenz8606@gmail.com>
2021-08-27 11:50:53 +0800

    feat: Add tests utils

---
+++ packages/tests/src/index.ts
@@ -0,0 +1,7 @@
+export { checkAccessibility } from './checkAccessibility';
+export { itRendersChildren } from './itRendersChildren';
+export { itSupportsClassName } from './itSupportsClassName';
+export { itSupportsOthers } from './itSupportsOthers';
+export { itSupportsRef } from './itSupportsRef';
+export { itSupportsStyle } from './itSupportsStyle';
+export { mockResizeObserver } from './mock-resize-observer';
