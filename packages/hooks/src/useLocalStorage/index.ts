d65f10206c150c2e0786da7e2795a76db92b73ce
chenz24 <chenz8606@gmail.com>
2021-08-27 11:29:42 +0800

    feat: Add useLocalStorage, useMediaQuery, useWindowEvent hooks

---
+++ packages/hooks/src/useLocalStorage/index.ts
@@ -0,0 +1,46 @@
+import { useState, useCallback, useEffect } from 'react';
+import { useWindowEvent } from '../useWindowEvent';
+
+export function useLocalStorage<T extends string>({
+  key,
+  defaultValue = undefined,
+}: {
+  key: string;
+  defaultValue?: T;
+}) {
+  const [value, setValue] = useState<T>(
+    typeof window !== 'undefined' && 'localStorage' in window
+      ? (window.localStorage.getItem(key) as T)
+      : ((defaultValue ?? '') as T)
+  );
+
+  const setLocalStorageValue = useCallback(
+    (val: T | ((prevState: T) => T)) => {
+      if (typeof val === 'function') {
+        setValue((current) => {
+          const result = val(current);
+          window.localStorage.setItem(key, result);
+          return result;
+        });
+      } else {
+        window.localStorage.setItem(key, val);
+        setValue(val);
+      }
+    },
+    [key]
+  );
+
+  useWindowEvent('storage', (event) => {
+    if (event.storageArea === window.localStorage && event.key === key) {
+      setValue(event.newValue as T);
+    }
+  });
+
+  useEffect(() => {
+    if (defaultValue && !value) {
+      setLocalStorageValue(defaultValue);
+    }
+  }, [defaultValue, value, setLocalStorageValue]);
+
+  return [value, setLocalStorageValue] as const;
+}
