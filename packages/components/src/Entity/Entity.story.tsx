7d8a1786f668aba6a515f8a72e31460e4d99334c
chenz24 <chenz8606@gmail.com>
2021-08-31 09:45:07 +0800

    feat: Add Entity component

---
+++ packages/components/src/Entity/Entity.story.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BadgeAnchor, Entity, Field, Tooltip, Text } from '@kubed/components';
+import { Error, Pod, Docker } from '@kubed/icons';
+
+export default {
+  title: 'Components/Entity',
+  component: Field,
+};
+
+const Avatar = (
+  <BadgeAnchor offset={[5, 5]}>
+    <Tooltip content="Badge information">
+      <Error className="badge" size={18} color="#fff" fill="rgb(245, 166, 35)" />
+    </Tooltip>
+    <Pod size={40} />
+  </BadgeAnchor>
+);
+
+export const Basic = () => <Field avatar={Avatar} label="存储类型: rocksdb" value="rocksdbpvc" />;
+
+export const EntityDemo = () => (
+  <Entity>
+    <Field avatar={Avatar} label="存储类型: rocksdb" value="rocksdbpvc" />
+    <Field label="存储卷" value="rocksdbpvc" />
+    <Field label="容量" value="1Gi" width={100} />
+    <Field label="访问模式" value="ReadWriteOnce" />
+  </Entity>
+);
+
+const footer = (
+  <>
+    <div style={{ display: 'inline-flex', minWidth: '200px', alignItems: 'center' }}>
+      <Docker size={20} style={{ marginRight: '8px' }} />
+      <Text weight={500}>moqlus-runtime</Text>
+    </div>
+    <div style={{ display: 'inline-flex', minWidth: '200px', alignItems: 'center' }}>
+      <Pod size={20} style={{ marginRight: '8px' }} />
+      <Text weight={500}>moqlus-runtime</Text>
+    </div>
+  </>
+);
+
+export const EntityHover = () => (
+  <Entity hoverable footer={footer}>
+    <Field avatar={Avatar} label="存储类型: rocksdb" value="rocksdbpvc" />
+    <Field label="存储卷" value="rocksdbpvc" />
+    <Field label="容量" value="1Gi" width={100} />
+    <Field label="访问模式" value="ReadWriteOnce" />
+  </Entity>
+);
---
+++ packages/components/src/Entity/Entity.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+import { Field } from './Field';
+import { DefaultProps, KubedTheme } from '../theme';
+import forwardRef from '../utils/forwardRef';
+import { addColorAlpha } from '../utils/color';
+
+export { Field };
+
+const getHoverStyle = (hoverable, theme: KubedTheme) => {
+  const { palette } = theme;
+  if (hoverable) {
+    return css`
+      border-color: ${palette.accents_5};
+      box-shadow: 0 4px 8px 0 ${addColorAlpha(palette.accents_8, 0.2)};
+    `;
+  }
+  return null;
+};
+
+const EntityWrapper = styled('div')<EntityProps>`
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  background-color: ${({ theme }) => theme.palette.background};
+  border: 1px solid ${({ theme }) => theme.palette.border};
+  border-radius: 4px;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    ${({ hoverable, theme }) => getHoverStyle(hoverable, theme)};
+  }
+`;
+
+const EntityContainer = styled('div')`
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 1;
+`;
+
+const EntityFooter = styled('div')`
+  display: flex;
+  border-radius: 4px;
+  padding: 8px;
+  margin-top: 12px;
+  background-color: ${({ theme }) => theme.palette.accents_1};
+`;
+
+export interface EntityProps extends DefaultProps {
+  /** Add effect on hover	 */
+  hoverable?: boolean;
+
+  /** Add effect on hover	 */
+  footer?: React.ReactNode;
+}
+
+export const Entity = forwardRef<EntityProps, 'div'>(({ children, footer, ...rest }, ref) => {
+  return (
+    <EntityWrapper ref={ref} {...rest}>
+      <EntityContainer>{children}</EntityContainer>
+      {footer && <EntityFooter>{footer}</EntityFooter>}
+    </EntityWrapper>
+  );
+});
+
+Entity.displayName = '@kubed/components/Entity';
