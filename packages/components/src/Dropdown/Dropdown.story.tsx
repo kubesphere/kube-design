c4007906c59f5fb7cbb6fa4cbad09b659a0edba3
chenz24 <chenz8606@gmail.com>
2021-08-31 16:51:41 +0800

    feat: Add Dropdown component.

---
+++ packages/components/src/Dropdown/Dropdown.story.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Dropdown, Menu, MenuItem, MenuLabel } from '@kubed/components';
+import { Add, Pen, Stop, Trash, More } from '@kubed/icons';
+
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown,
+};
+
+const MenuComponent = (
+  <Menu>
+    <MenuLabel>menu label</MenuLabel>
+    <MenuItem icon={<Add />}>创建</MenuItem>
+    <MenuItem icon={<Stop />}>停止调度</MenuItem>
+    <MenuItem icon={<Pen />}>Edit</MenuItem>
+    <MenuItem icon={<Trash />}>Delete</MenuItem>
+  </Menu>
+);
+
+export const Basic = () => (
+  <div style={{ height: '300px', width: '100%', backgroundColor: '#f9fbfd' }}>
+    <Dropdown content={MenuComponent}>
+      <Button variant="text" radius="lg">
+        <More size={16} />
+      </Button>
+    </Dropdown>
+  </div>
+);
