463f5e87e6159ae8c1564254a029e9a24f3c1260
chenz24 <chenz8606@gmail.com>
2021-09-12 08:29:27 +0800

    feat: Add Radio component

---
+++ packages/components/src/Radio/RadioGroup.tsx
@@ -0,0 +1,58 @@
+import React, { useMemo, useEffect } from 'react';
+import { useUncontrolled } from '@kubed/hooks';
+import isUndefined from 'lodash/isUndefined';
+import { Group } from '../Group/Group';
+import { RadioContext } from './RadioContext';
+
+interface Props {
+  value?: string | number;
+  defaultValue?: string;
+  disabled?: boolean;
+  onChange?: (value) => void;
+}
+
+type NativeAttrs = Omit<React.HTMLAttributes<any>, keyof Props>;
+export type RadioGroupProps = Props & NativeAttrs;
+
+export const RadioGroup: React.FC<React.PropsWithChildren<RadioGroupProps>> = ({
+  onChange,
+  disabled,
+  value,
+  defaultValue,
+  children,
+}: RadioGroupProps) => {
+  const [_value, setValue] = useUncontrolled({
+    value,
+    defaultValue,
+    finalValue: '',
+    rule: (val) => !isUndefined(val),
+    onChange,
+  });
+
+  const updateState = (val) => {
+    if (!isUndefined(value)) return;
+    setValue(val);
+  };
+
+  const providerValue = useMemo(() => {
+    return {
+      updateState,
+      disabledAll: disabled,
+      inGroup: true,
+      value: _value,
+    };
+  }, [disabled, _value]);
+
+  useEffect(() => {
+    if (isUndefined(value)) return;
+    setValue(value);
+  }, [value]);
+
+  return (
+    <RadioContext.Provider value={providerValue}>
+      <Group>{children}</Group>
+    </RadioContext.Provider>
+  );
+};
+
+RadioGroup.displayName = '@kubed/components/RadioGroup';
