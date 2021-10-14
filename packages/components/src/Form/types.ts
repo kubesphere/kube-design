354fb3e0361b3162505795c985209eb1cf1ba042
chenz24 <wayne@kubesphere.io>
2021-10-14 21:25:45 +0800

    fix: Fix lint error in docs

--- packages/components/src/Form/types.ts
+++ packages/components/src/Form/types.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
 export { Options as ScrollOptions } from 'scroll-into-view-if-needed';
 export type FormLabelAlign = 'left' | 'right';
+// @ts-ignore
 export { Store, StoreValue, NamePath, InternalNamePath } from 'rc-field-form/lib/interface';
