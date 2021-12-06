38d503e994010cccda23ea868c93e30112474186
chenz24 <wayne@kubesphere.io>
2021-12-06 20:53:50 +0800

    feat: Add useMove hook

--- packages/hooks/src/utils/index.ts
+++ packages/hooks/src/utils/index.ts
@@ -1,3 +1,13 @@
 export function randomId() {
   return `kubed-${Math.random().toString(36).substr(2, 9)}`;
 }
+
+interface Clamp {
+  value: number;
+  min: number;
+  max: number;
+}
+
+export function clamp({ value, min, max }: Clamp) {
+  return Math.min(Math.max(value, min), max);
+}
