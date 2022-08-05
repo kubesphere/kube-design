e0bd214e62371da90bb7ee717fb3aec47916b50c
chenz24 <wayne@kubesphere.io>
2022-08-05 23:19:13 +0800

    feat: Add another circle loader
    Signed-off-by: chenz24 <wayne@kubesphere.io>

--- packages/components/src/Loading/Loading.story.tsx
+++ packages/components/src/Loading/Loading.story.tsx
@@ -13,3 +13,13 @@ export const Basic = () => (
     <Loading size="lg" />
   </Group>
 );
+
+export const CircleLoading2 = () => (
+  <Group>
+    <Loading size="xs" variant="circle2" />
+    <Loading size="sm" variant="circle2" />
+    <Loading size="md" variant="circle2" />
+    <Loading size="lg" variant="circle2" />
+    <Loading size="md" variant="circle2" color="warning" />
+  </Group>
+);
