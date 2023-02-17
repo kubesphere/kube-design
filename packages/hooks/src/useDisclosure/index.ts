1576c58579736e11a02c47494ac4785f12ffec4b
donniean <acmilank22@gmail.com>
2023-02-17 10:16:54 +0800

    feat: useDisclosure

--- packages/hooks/src/useDisclosure/index.ts
+++ packages/hooks/src/useDisclosure/index.ts
@@ -1,13 +1,13 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export function useDisclosure(initialIsOpen = false) {
   const [isOpen, setIsOpen] = useState(initialIsOpen);
 
-  const open = () => setIsOpen(true);
+  const open = useCallback(() => setIsOpen(true), []);
 
-  const close = () => setIsOpen(false);
+  const close = useCallback(() => setIsOpen(false), []);
 
-  const toggle = () => (isOpen ? close() : open());
+  const toggle = useCallback(() => (isOpen ? close() : open()), [isOpen, close, open]);
 
   return { isOpen, open, close, toggle };
 }
