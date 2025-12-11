2fde3d3d7b63e800e77326df0ad7268873813504
leioy <wlbyan@icloud.com>
2025-12-11 18:06:52 +0800

    chore: Change Docusaurus baseUrl from `/kube-design/` to `/`
    Signed-off-by: leioy <wlbyan@icloud.com>

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://kubesphere.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kube-design/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
